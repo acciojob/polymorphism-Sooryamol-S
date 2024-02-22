5831b6f329e734bc2ac0d4c7e6a7de6733c36e41
Sooryamol-S <sooryamolsivapuri@gmail.com>
2024-02-22 12:17:30 +0530

    changes are made

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -4,5 +4,25 @@ import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
-     
+
+    @Test
+    public void testProductWithTwoIntegers() {
+        Main.Product p = new Main.Product();
+        int result = p.product(2, 3);
+        assertEquals(6, result);
+    }
+
+    @Test
+    public void testProductWithThreeIntegers() {
+        Main.Product p = new Main.Product();
+        int result = p.product(2, 3, 4);
+        assertEquals(24, result);
+    }
+
+    @Test
+    public void testProductWithTwoDoubles() {
+        Main.Product p = new Main.Product();
+        double result = p.product(2.5, 3.5);
+        assertEquals(8.75, result, 0.0001); // Use a delta value for double comparisons
+    }
 }
